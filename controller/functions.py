7db4c8ba80ea63aa479608ae875e35d98df0ce6b
eduardo <dumedolago@gmail.com>
2025-04-03 16:10:39 -0300

    interface adjustments and some errors corrections

--- controller/functions.py
+++ controller/functions.py
@@ -5,31 +5,35 @@ import flet as ft
 alfabetoMins = string.ascii_lowercase
 alfabetoMinsMaiusc = string.ascii_uppercase                                                               
 numeros = string.digits
-simbolos = string.punctuation
+simbolos = '@#$%&*!'
 tudo  = [alfabetoMins, alfabetoMinsMaiusc, numeros, simbolos]
 
-qtde = 3
-senha = []
-
 def gerar_senha(e, page, st, erro):
+
+    senha = []
+    
     try:
-        if int(e)<4:
+        qtde = int(e)
+        if qtde<4:
             erro.value = f'sua senha deve ter mais de 4 caracteres!'
             page.update()
+            return
 
-        else:   
-            while len(senha)<int(e):
-                for x in tudo:
-                    ch = random.choice(x)
-                    senha.append(ch)
-                    if len(senha)==int(e):
+        for grupo in tudo:
+            senha.append(random.choice(grupo)) 
+        while len(senha)<qtde:
+            for x in tudo:
+                ch = random.choice(x)
+                senha.append(ch)
+                if len(senha)==qtde:
                         break
-            random.shuffle(senha)
-            senha_s = ', '.join(senha).replace(', ', '')
-            erro.value = ""
-            st.value = f'Sua senha: {senha_s}'
-            page.update()
-            print('botao clicado')
+        random.shuffle(senha)
+        senha_s = ', '.join(senha).replace(', ', '')
+        erro.value = ""
+        st.value = f'Sua senha: {senha_s}'
+        page.update()
+
     except ValueError:
+        st.value = f''
         erro.value = f'insira um número!'
         page.update()
--- view/interface.py
+++ view/interface.py
@@ -3,17 +3,39 @@ from controller.functions import gerar_senha
 
 def main(page: ft.Page):
     page.title = "Gerador de Senhas Seguras"
-    instrucoes = ft.Text('No campo abaixo, insira a quantidade de caracteres que a senha deve ter:')
-    qtde = ft.TextField(label='digite um valor...')
-    senha_texto = ft.Text("")
-    erro = ft.Text("")
-    gerar = ft.ElevatedButton('Gerar senha', on_click=lambda e: gerar_senha(qtde.value, page, senha_texto, erro))
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    instrucoes = ft.Text(
+        'No campo abaixo, insira a quantidade de caracteres que a senha deve ter:',
+        text_align="center"
+    )
+
+    qtde = ft.TextField(
+        label='digite um valor...',
+        width=200,
+        text_align="center"
+    )
+
+    senha_texto = ft.Text("", text_align="center")
+    erro = ft.Text("", text_align="center")
+
+    gerar = ft.ElevatedButton(
+        'Gerar senha', 
+        on_click=lambda e: gerar_senha(qtde.value, page, senha_texto, erro)
+    )
 
     page.add(
-        instrucoes, 
-        qtde,
-        gerar,
-        senha_texto,
-        erro
-        
+        ft.Column(
+            [
+                instrucoes, 
+                qtde,
+                gerar,
+                senha_texto,
+                erro
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            spacing=10
+        )
     )
