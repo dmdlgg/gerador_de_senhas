bfaf2631c8c778d98cc7192c5ff3576884823ef5
eduardo <dumedolago@gmail.com>
2025-04-03 15:21:11 -0300

    implementing logic with interface

--- controller/functions.py
+++ controller/functions.py
@@ -1,21 +1,29 @@
 import random 
 import string
- 
+import flet as ft
+
 alfabetoMins = string.ascii_lowercase
 alfabetoMinsMaiusc = string.ascii_uppercase                                                               
 numeros = string.digits
 simbolos = string.punctuation
 tudo  = [alfabetoMins, alfabetoMinsMaiusc, numeros, simbolos]
 
-qtde = 8
+qtde = 3
 senha = []
 
-while len(senha)<qtde:
-    for x in tudo:
-        ch = random.choice(x)
-        senha.append(ch)
-        if len(senha)==qtde:
-            break
-random.shuffle(senha)
-senha_s = ', '.join(senha).replace(', ', '')
-print(senha_s)
+def gerar_senha(e, page, st):
+    
+    if int(e)<4:
+        raise ValueError('A quantidade minima de caracteres sao 4!')
+
+    while len(senha)<int(e):
+        for x in tudo:
+            ch = random.choice(x)
+            senha.append(ch)
+            if len(senha)==int(e):
+                break
+    random.shuffle(senha)
+    senha_s = ', '.join(senha).replace(', ', '')
+    st.value = f'Sua senha: {senha_s}'
+    page.update()
+    print('botao clicado')
--- view/interface.py
+++ view/interface.py
@@ -1,12 +1,18 @@
 import flet as ft
+from controller.functions import gerar_senha
 
 def main(page: ft.Page):
     page.title = "Gerador de Senhas Seguras"
     instrucoes = ft.Text('No campo abaixo, insira a quantidade de caracteres que a senha deve ter:')
     qtde = ft.TextField(label='digite um valor...')
-    gerar_senha = ft.ElevatedButton('Gerar senha')
+    senha_texto = ft.Text("")
+    gerar = ft.ElevatedButton('Gerar senha', on_click=lambda e: gerar_senha(qtde.value, page, senha_texto))
+    
 
     page.add(
         instrucoes, 
-        qtde
+        qtde,
+        gerar,
+        senha_texto
+        
     )
